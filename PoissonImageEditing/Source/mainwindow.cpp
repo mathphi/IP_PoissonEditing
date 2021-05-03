01423b5bf395998c7db56d48f0bfda9c0a9023b8
Mathieu Philippart <philippartmathieu@gmail.com>
2021-05-03 20:11:49 +0200

    Added source/target image opening and display

--- PoissonImageEditing/Source/mainwindow.cpp
+++ PoissonImageEditing/Source/mainwindow.cpp
@@ -1,15 +1,104 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QFileDialog>
+#include <QImageReader>
+#include <QImageWriter>
+
+#include <QDebug>
+
+#define IMAGE_EXTENSIONS "PNG (*.png);;JPG (*.jpg *.jpeg);;BMP (*.bmp);;TIFF (*.tif *.tiff)"
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    m_scene_source = new QGraphicsScene(this);
+    m_scene_target = new QGraphicsScene(this);
+
+    ui->graphicsViewSource->setScene(m_scene_source);
+    ui->graphicsViewTarget->setScene(m_scene_target);
+
+    m_pix_item_source = new QGraphicsPixmapItem;
+    m_pix_item_target = new QGraphicsPixmapItem;
+
+    m_scene_source->addItem(m_pix_item_source);
+    m_scene_target->addItem(m_pix_item_target);
+
+    connect(ui->actionQuit, SIGNAL(triggered(bool)), this, SLOT(close()));
+
+    connect(ui->actionOpen_source_image, SIGNAL(triggered(bool)), this, SLOT(openSourceImage()));
+    connect(ui->actionOpen_target_image, SIGNAL(triggered(bool)), this, SLOT(openTargetImage()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete m_pix_item_source;
+    delete m_pix_item_target;
 }
 
+
+void MainWindow::resizeEvent(QResizeEvent *e) {
+    Q_UNUSED(e);
+
+    // Fit graphics views to the pixmap items
+    ui->graphicsViewSource->fitInView(m_pix_item_source, Qt::KeepAspectRatio);
+    ui->graphicsViewTarget->fitInView(m_pix_item_target, Qt::KeepAspectRatio);
+}
+
+
+void MainWindow::openSourceImage() {
+    // Open existing image file
+    QString filename = QFileDialog::getOpenFileName(
+                this,
+                "Open a source image",
+                QDir::homePath(),
+                IMAGE_EXTENSIONS);
+
+    // If dialog was closed
+    if (filename.isEmpty())
+        return;
+
+    m_source_image = QImage(filename);
+
+    updateSourceScene();
+}
+
+void MainWindow::openTargetImage() {
+    // Open existing image file
+    QString filename = QFileDialog::getOpenFileName(
+                this,
+                "Open a target image",
+                QDir::homePath(),
+                IMAGE_EXTENSIONS);
+
+    // If dialog was closed
+    if (filename.isEmpty())
+        return;
+
+    m_target_image = QImage(filename);
+
+    updateTargetScene();
+}
+
+void MainWindow::updateSourceScene() {
+    // Update the picture of the pixmap item
+    m_pix_item_source->setPixmap(QPixmap::fromImage(m_source_image));
+
+    // Fit graphics view to the pixmap item
+    ui->graphicsViewSource->fitInView(m_pix_item_source, Qt::KeepAspectRatio);
+}
+
+void MainWindow::updateTargetScene() {
+    // Update the picture of the pixmap item
+    m_pix_item_target->setPixmap(QPixmap::fromImage(m_target_image));
+
+    // Fit graphics view to the pixmap item
+    ui->graphicsViewTarget->fitInView(m_pix_item_target, Qt::KeepAspectRatio);
+}
--- PoissonImageEditing/Source/mainwindow.h
+++ PoissonImageEditing/Source/mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 
+class QGraphicsScene;
+class QGraphicsPixmapItem;
+
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,7 +19,26 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void resizeEvent(QResizeEvent *e) override;
+
+private slots:
+    void openSourceImage();
+    void openTargetImage();
+
+    void updateSourceScene();
+    void updateTargetScene();
+
 private:
     Ui::MainWindow *ui;
+
+    QGraphicsScene *m_scene_source;
+    QGraphicsScene *m_scene_target;
+
+    QGraphicsPixmapItem *m_pix_item_source;
+    QGraphicsPixmapItem *m_pix_item_target;
+
+    QImage m_source_image;
+    QImage m_target_image;
 };
 #endif // MAINWINDOW_H
