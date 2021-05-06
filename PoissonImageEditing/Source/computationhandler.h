aebf2d8c57817f614eaedf4b2995bab6e364738e
Mathieu Philippart <philippartmathieu@gmail.com>
2021-05-06 20:06:51 +0200

    Added tests with SparseMatrix class (Eigen)

--- PoissonImageEditing/Source/computationhandler.h
+++ PoissonImageEditing/Source/computationhandler.h
@@ -4,12 +4,14 @@
 #include <QObject>
 
 #include <Eigen/Core>
+#include <Eigen/Sparse>
 
 
 /*
  * Eigen matrices type definitions
  */
 typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> MatrixXd;
+typedef Eigen::SparseMatrix<float> SparseMatrixXd;
 
 
 class ComputationHandler : public QObject
